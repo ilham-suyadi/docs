645fa49b965938f06db75f7dc9d2210341327bca
ilham.suyadi <ilham.suyadi@solusi247.com>
2023-05-06 18:43:01 +0700

    deployment

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: "/docs/",
+  base: '/docs/',
   title: "Ilham Suyadi",
   description: "My Documentation",
   themeConfig: {
