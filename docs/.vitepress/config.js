c27e388d94b776cc209885665a4ce42edd5dcfd8
ilham.suyadi <ilham.suyadi@solusi247.com>
2023-05-06 19:39:50 +0700

    fixing deployment

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/docs/',
-  outDir: "docs/.vitepress/config.js__..public",
+  base: '/',
   title: "Ilham Suyadi",
   description: "My Documentation",
   themeConfig: {
